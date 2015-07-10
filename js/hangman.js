b2d5ebc563d3072b9432b2bbc6ddc18cc1e9f016
Philip Engles <philipengles@Philips-MacBook-Pro-2.local>
2015-07-10 15:32:22 -0400

    first attempt

---
+++ js/hangman.js
@@ -0,0 +1,19 @@
+$( document ).ready(function(){
+//player will be prompted to enter word
+var word = prompt("What is your word?");
+//word will be stored and hidden
+var letter = $("#letter").html(word)
+var letter = $("#letter").html(word)
+
+//having blank space lines appear for word
+//take player 2 input on.(click)
+//if statement on whether letter is present or not
+//if it is present, show letter within word
+//else subtract 1 from counter
+console.log($.inArray("cat", ['c', 'a', 't']) > -1);
+
+
+})
+
+
+//$(".game-word").html(word);
